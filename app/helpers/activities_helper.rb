c1c7e014c543d754cd47bad6a38aa026a0bf68a3
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-09-24 16:29:31 +0200

    Activities views and helper

---
+++ app/helpers/activities_helper.rb
@@ -0,0 +1,10 @@
+module ActivitiesHelper
+
+  def like_activity(activity)
+    if (activity.liked_by?(current_user))
+      link_to t('activity.unlike'), activity_like_path(activity), :method => :delete, :remote => true
+    else
+      link_to t('activity.like'), activity_like_path(activity), :method => :post, :remote => true;
+    end
+  end
+end
