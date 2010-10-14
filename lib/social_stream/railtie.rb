e54c3cd19ff7f995a0d1a614708928f58d5486bb
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-10-14 17:49:13 +0200

    Enforce devise support

--- lib/social_stream/railtie.rb
+++ lib/social_stream/railtie.rb
@@ -1,5 +1,7 @@
 module SocialStream
   class Railtie < Rails::Railtie
+    config.generators.authentication :devise
+
     config.to_prepare do
       %w( actor activity_object ).each do |supertype|
         supertype.classify.constantize.load_subtype_features
