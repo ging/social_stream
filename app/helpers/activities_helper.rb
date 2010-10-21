d40564c23422dff540c126845f56fe215169e460
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-10-21 17:44:34 +0200

    Pulish ActivitiesHelper

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,6 +1,10 @@
 module ActivitiesHelper
 
-  def like_activity(activity)
+  # Link to 'like' or 'unlike' depending on the like status of the activity to current_user
+  # 
+  # @param [Activity]
+  # @return [String]
+  def link_like(activity)
     if (activity.liked_by?(current_user))
       link_to t('activity.unlike'), activity_like_path(activity), :method => :delete, :remote => true
     else
