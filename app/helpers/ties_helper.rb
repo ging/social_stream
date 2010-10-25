436c985aabb7430c4cc3a17a7a5545c0ca4683b5
Diego Carrera <diegocarrera2000@gmail.com>
2010-10-25 13:53:00 +0200

    changes on ties

--- app/helpers/ties_helper.rb
+++ app/helpers/ties_helper.rb
@@ -9,12 +9,19 @@ module TiesHelper
     raise "Relation not provided for #{ subject.class.to_s.underscore }" if relation.blank?
 
     if relation=='follower'
-      link_to t("tie.suggestion.#{ relation }.new"),
-              new_tie_path("tie[sender_id]" => current_user.actor.id,
-                           "tie[receiver_id]" => subject.actor.id,
-                           "tie[relation_name]" => relation),
-              :class => 'boxy',
-              :title => t("tie.suggestion.#{ relation }.confirm_new", :name => subject.name)
+        form_for Tie.new(:sender_id => current_user.actor.id,
+                         :receiver_id => subject.actor.id,
+                         :relation_name => relation ),
+                      :remote => true do |f|
+             f.hidden_field :receiver_id
+             f.hidden_field :sender_id
+             f.hidden_field :relation_name
+             f.submit t('follow'), :class => "follow_btn"
+        end
+
+
+
+
     else
       link_to t("tie.suggestion.#{ relation }.new"),
               new_tie_path("tie[sender_id]" => current_user.actor.id,
