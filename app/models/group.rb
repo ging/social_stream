fc74a4ab4176112e2fa178c4455a816a0744f727
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-01-14 15:59:52 +0100

    Scaffold groups creation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,16 @@
 class Group < ActiveRecord::Base
+  attr_accessor :_founder
+
   def followers
     subjects(:subject_type => :user, :direction => :senders)
+  end
+
+  after_create :create_founder
+
+  private
+
+  def create_founder
+    sent_ties.create! :receiver => Actor.find_by_permalink!(_founder),
+                      :relation => relations.sort.first
   end
 end
--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,3 +1,4 @@
 Factory.define :group do |g|
   g.sequence(:name) { |n| "Group #{ n }" }
+  g._founder { |g| Factory(:user).permalink }
 end
