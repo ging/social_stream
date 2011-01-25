cd63a21192edabb3895a909b5ae41aa40c3eed1e
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-01-25 18:20:01 +0100

    Cache current_subject

--- lib/social_stream/controllers/helpers.rb
+++ lib/social_stream/controllers/helpers.rb
@@ -11,14 +11,17 @@ module SocialStream
       module InstanceMethods
         # Current subject represented by the user. Defaults to the own user
         def current_subject
-          current_subject_from_session ||
-            current_user
+          @current_subject ||=
+            current_subject_from_session ||
+              current_user
         end
 
         # Set represented subject
         def current_subject= instance
           session[:subject_type] = instance.class.to_s
           session[:subject_id]   = instance.id
+
+          @current_subject = instance
         end
 
         private
