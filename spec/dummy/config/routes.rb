7be582e5e94bc04d64f6f2b6375abb91500a768a
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-03-14 17:51:04 +0100

    Include devise route in spec/dummy

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Dummy::Application.routes.draw do
+  devise_for :users, :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
