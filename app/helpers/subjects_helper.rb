8c754da292a909b5734606ea264ca417a18f6c7c
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-03-24 11:30:33 +0100

    Documentation

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -6,6 +6,24 @@ module SubjectsHelper
   #
   # The menu option allows overwriting a menu slot with the content of the given block
   #
+  # Examples:
+  #
+  # Render the home toolbar:
+  #
+  #   <% toolbar %>
+  #
+  # Render the profile toolbar for a user:
+  #
+  #   <% toolbar :profile => @user %>
+  #
+  # Render the home toolbar changing the messages menu option:
+  #
+  #   <% toolbar :option => :messages %>
+  #
+  # Render the profile toolbar for group changing the contacts menu option:
+  #
+  #   <% toolbar :profile => @group, :option => :contacts %>
+  #
   def toolbar(options = {}, &block)
     if options[:option] && block_given?
       menu_options[options[:option]] = capture(&block)
@@ -20,6 +38,9 @@ module SubjectsHelper
     end
   end
 
+  # Cache menu options for toolbar
+  #
+  # @api private
   def menu_options #:nodoc:
     @menu_options ||= {}
   end
