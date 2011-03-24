71b530e923989b1f47e64b1d463e0da3b1cef187
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-03-24 17:32:01 +0100

    Include Mailboxer migrations in spec

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,9 +1,18 @@
-# Use migration template
+# Use Social Stream migration template
 path = %w( .. .. .. lib generators social_stream templates migration )
 path.unshift File.dirname(__FILE__)
 
 require File.join(*path)
 
+# Use Mailboxer migration template
+finder = Gem::GemPathSearcher.new
+mailboxer_spec = finder.find('mailboxer')
+mailboxer_migration =
+  finder.matching_files(mailboxer_spec,
+                        File.join("generators", "mailboxer", "templates", "migration")).first
+require mailboxer_migration
+
 ActiveRecord::Schema.define(:version => 0) do
+  CreateMailboxer.up
   CreateSocialStream.up
 end
--- spec/support/db.rb
+++ spec/support/db.rb
@@ -1,11 +1,23 @@
-require File.join(Rails.root, '../../lib/generators/social_stream/templates/migration')
+# Social Stream migration template
+require File.join(Rails.root, File.join('..', '..', 'lib', 'generators', 'social_stream', 'templates', 'migration'))
 
-begin
-  CreateSocialStream.down
-rescue
-  puts "WARNING: SocialStream migration failed to rollback"
+# Mailboxer migration template
+finder = Gem::GemPathSearcher.new
+mailboxer_spec = finder.find('mailboxer')
+mailboxer_migration =
+  finder.matching_files(mailboxer_spec,
+                        File.join('generators', 'mailboxer', 'templates', 'migration')).first
+require mailboxer_migration
+
+%w( SocialStream Mailboxer ).each do |m|
+  begin
+    "Create#{ m }".constantize.down
+  rescue
+    puts "WARNING: #{ m } migration failed to rollback" 
+  end
 end
 
+CreateMailboxer.up
 CreateSocialStream.up
 
 require File.expand_path("../../dummy/db/seeds", __FILE__)
