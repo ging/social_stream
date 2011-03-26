c253f43409068a6a136b3cb38ceb2d52d5bf7f87
Jaime Castro Montero <jaime1988@gmail.com>
2011-03-27 00:40:13 +0100

    Avatar current_subject change bug fixed

---
+++ app/controllers/avatars_controller.rb
@@ -0,0 +1,37 @@
+class AvatarsController < InheritedResources::Base
+=begin  
+  def new
+#    if params[:logo_logo].present?
+ #    render :template => "logos/_precrop", :layout => false
+  #  end
+    debugger
+    if params[:logo_logo]
+      
+    end
+     respond_to do |format|
+      format.html #new.html.erb
+     end
+ 
+  end
+=end
+
+def create
+    @avatar = Avatar.create(params[:logo])
+    
+    
+    if @avatar.new_record?
+      render :new
+    else
+     # debugger
+      @avatar.updating_logo = true
+      @avatar.actor_id = current_subject.actor_id
+      if !current_subject.avatar.nil?
+      	current_subject.avatar.destroy
+      end
+      @avatar.save     
+      redirect_to avatars_path
+    end    
+  end
+  
+  
+end
