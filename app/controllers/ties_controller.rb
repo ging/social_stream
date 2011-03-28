ce223160a8a0b9797247585fe91240b95c664e01
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-03-28 15:07:51 +0200

    Rearrange TiesController with authentication filter

--- app/controllers/ties_controller.rb
+++ app/controllers/ties_controller.rb
@@ -1,17 +1,16 @@
 class TiesController < InheritedResources::Base
   respond_to :html, :xml, :js
 
-  before_filter :authenticate_user!, :only => :suggestion
+  before_filter :authenticate_user!
+
+  def create
+    create! do |format|
+      format.html { redirect_to resource.receiver_subject }
+    end
+  end
 
   def suggestion
     @tie = current_subject.suggestion
     render :layout  => false
   end
-  
-  def create
-      super do |format|
-        format.html { redirect_to resource.receiver_subject }
-      end
-  end
-  
 end
