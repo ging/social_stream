0475bfbc21ed2630c214457625fef2b01fd74bcb
Eduardo Casanova Cuesta <ecasanovac@gmail.com>
2011-04-06 00:53:58 +0200

    Issue #55 solved

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,11 +1,6 @@
 class AvatarsController < InheritedResources::Base
 
   before_filter :authenticate_user!
-  
-  def index
-  	@avatar = Avatar.new
-  end
-  
   def update
 
     if !current_subject.avatars.blank?
@@ -13,22 +8,22 @@ class AvatarsController < InheritedResources::Base
       new_logo = Avatar.find(params[:id])
 
       if (new_logo.actor == current_subject.actor)
-        
+
         actual_logo = current_subject.avatars.active.first
         if !actual_logo.blank?
-          actual_logo.active = false
-          actual_logo.save
+        actual_logo.active = false
+        actual_logo.save
         end
-  
-        new_logo.active = true
-        new_logo.save
+
+      new_logo.active = true
+      new_logo.save
       end
     end
     redirect_to avatars_path
   end
 
   def create
-    @avatar = Avatar.create(params[:logo])
+    @avatar = Avatar.create(params[:avatar])
 
     if @avatar.new_record?
       render :new
@@ -37,14 +32,20 @@ class AvatarsController < InheritedResources::Base
       @avatar.actor_id = current_subject.actor.id
       if !current_subject.avatars.blank?
         actual_logo = current_subject.avatars.active.first
-        actual_logo.active = false
-        actual_logo.save
+      actual_logo.active = false
+      actual_logo.save
       end
       @avatar.active = true
       @avatar.save
       redirect_to avatars_path
-      #redirect_to [current_subject, :profile]
+    #redirect_to [current_subject, :profile]
     end
+  end
+
+  protected
+
+  def begin_of_association_chain
+    current_subject
   end
 
 end
