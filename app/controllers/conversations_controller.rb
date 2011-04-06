7c340ae96ffaf8c91d70d168e0d26c0ac9e98f26
Eduardo Casanova Cuesta <ecasanovac@gmail.com>
2011-04-06 11:26:47 +0200

    Adding and refactoring conversations filters

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,107 +1,102 @@
 class ConversationsController < ApplicationController
 
-	before_filter :get_mailbox, :get_box, :get_actor
-	def index
-		if @box.eql?"inbox"
-			@conversations = @mailbox.inbox.paginate(:per_page => 9, :page => params[:page])
-		elsif @box.eql?"sentbox"
-			@conversations = @mailbox.sentbox.paginate(:per_page => 9, :page => params[:page])
-		else
-			@conversations = @mailbox.trash.paginate(:per_page => 9, :page => params[:page])
-		end
-	end
+  before_filter :authenticate_user!
+  before_filter :get_mailbox, :get_box, :get_actor
+  before_filter :check_current_subject_in_conversation, :only => [:show, :update, :destroy]
+  def index
+    if @box.eql?"inbox"
+      @conversations = @mailbox.inbox.paginate(:per_page => 9, :page => params[:page])
+    elsif @box.eql?"sentbox"
+      @conversations = @mailbox.sentbox.paginate(:per_page => 9, :page => params[:page])
+    else
+      @conversations = @mailbox.trash.paginate(:per_page => 9, :page => params[:page])
+    end
+  end
 
-	def show
-		@conversation = Conversation.find_by_id(params[:id])
-		if @conversation.nil? or !@conversation.is_participant?(@actor)
-			redirect_to conversations_path(:box => @box)
-		return
-		end
-		if @box.eql? 'trash'
-		@receipts = @conversation.receipts(@actor).trash
-		else
-		@receipts = @conversation.receipts(@actor).not_trash
-		end
-		render :action => :show
-		@receipts.mark_as_read
-	end
+  def show
+    if @box.eql? 'trash'
+      @receipts = @conversation.receipts(@actor).trash
+    else
+      @receipts = @conversation.receipts(@actor).not_trash
+    end
+    render :action => :show
+    @receipts.mark_as_read
+  end
 
-	def new
+  def new
 
-	end
+  end
 
-	def edit
+  def edit
 
-	end
+  end
 
-	def create
+  def create
 
-	end
+  end
 
-	def update
-		@conversation = Conversation.find_by_id(params[:id])
-		if @conversation.nil? or !@conversation.is_participant?(@actor)
-			redirect_to conversations_path(:box => @box)
-		return
-		end
+  def update
+    if params[:untrash].present?
+    @conversation.untrash(@actor)
+    end
 
-		if params[:untrash].present?
-		@conversation.untrash(@actor)
-		end
+    if params[:reply_all].present?
+      last_receipt = @conversation.receipts(@actor).last
+      @receipt = @actor.reply_to_all(last_receipt, params[:body])
+    end
 
-		if params[:reply_all].present?
-			last_receipt = @conversation.receipts(@actor).last
-			@receipt = @actor.reply_to_all(last_receipt, params[:body])
-		end
+    if @box.eql? 'trash'
+      @receipts = @conversation.receipts(@actor).trash
+    else
+      @receipts = @conversation.receipts(@actor).not_trash
+    end
+    render :action => :show
+    @receipts.mark_as_read
 
-		if @box.eql? 'trash'
-		@receipts = @conversation.receipts(@actor).trash
-		else
-		@receipts = @conversation.receipts(@actor).not_trash
-		end
-		render :action => :show
-		@receipts.mark_as_read
+  end
 
-	end
+  def destroy
 
-	def destroy
-		@conversation = Conversation.find_by_id(params[:id])
-		if @conversation.nil? or !@conversation.is_participant?(@actor)
-			redirect_to conversations_path(:box => @box)
-		return
-		end
+    @conversation.move_to_trash(@actor)
 
-		@conversation.move_to_trash(@actor)
+    if params[:location].present?
+      case params[:location]
+      when 'conversation'
+        redirect_to conversations_path(:box => :trash)
+        return
+      else
+      redirect_to conversations_path(:box => @box,:page => params[:page])
+      return
+      end
+    end
+    redirect_to conversations_path(:box => @box,:page => params[:page])
+  end
 
-		if params[:location].present?
-			case params[:location]
-			when 'conversation'
-				redirect_to conversations_path(:box => :trash)
-				return
-			else
-			redirect_to conversations_path(:box => @box,:page => params[:page])
-			return
-			end
-		end
-		redirect_to conversations_path(:box => @box,:page => params[:page])
-	end
+  private
 
-	private
+  def get_mailbox
+    @mailbox = current_subject.mailbox
+  end
 
-	def get_mailbox
-		@mailbox = current_subject.mailbox
-	end
+  def get_actor
+    @actor = Actor.normalize(current_subject)
+  end
 
-	def get_actor
-		@actor = Actor.normalize(current_subject)
-	end
+  def get_box
+    if params[:box].blank? or !["inbox","sentbox","trash"].include?params[:box]
+      @box = "inbox"
+    return
+    end
+    @box = params[:box]
+  end
 
-	def get_box
-		if params[:box].blank? or !["inbox","sentbox","trash"].include?params[:box]
-			@box = "inbox"
-		return
-		end
-		@box = params[:box]
-	end
+  def check_current_subject_in_conversation
+    @conversation = Conversation.find_by_id(params[:id])
+
+    if @conversation.nil? or !@conversation.is_participant?(@actor)
+      redirect_to conversations_path(:box => @box)
+    return
+    end
+  end
 
 end
