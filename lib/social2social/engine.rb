e58da4e903c8b41684382d3b83cd9cbc7d92f40f
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-04-25 10:30:12 +0200

    Reorganize Engine

--- lib/social2social/engine.rb
+++ lib/social2social/engine.rb
@@ -3,23 +3,19 @@ module Social2social
     config.app_generators.social_stream :social_stream
 
     config.to_prepare do
-      #Loading RemoteUser as SocialStream Subject
-      SocialStream.subjects << :remote_subject
-      ::Actor.load_subtype_features
-      
       #Patching Actor
-      class ::Actor
+      Actor.class_eval do
         include Social2social::Models::Shareable
       end
       
-      #Forcing preload of TieActivity
-      TieActivity      
-      
       #Patching TieAct
-      class ::TieActivity
+      TieActivity.class_eval do
         include Social2social::Models::UpdateTriggerable
       end
     end
-        
+
+    initializer "social2social.remote_subject_in_social_stream_subjects" do
+      SocialStream.subjects << :remote_subject unless SocialStream.subjects.include?(:remote_subject)
+    end
   end
 end
