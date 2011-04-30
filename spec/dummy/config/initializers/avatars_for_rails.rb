0094b26fc7363ed5c422b7b801e419766a162aa5
Jaime Castro Montero <jaime1988@gmail.com>
2011-04-30 13:05:58 +0200

    multiple avatar styles

--- spec/dummy/config/initializers/avatars_for_rails.rb
+++ spec/dummy/config/initializers/avatars_for_rails.rb
@@ -2,4 +2,8 @@ AvatarsForRails.setup do |config|
   config.avatarable_model = :actor
   config.current_avatarable_object = :current_actor
   config.avatarable_filters = [:authenticate_user!]
+  config.avatarable_styles = { :representation => "20x20>",
+                                   :tie => "30x30>",
+                                   :actor => '35x35>',
+                                   :profile => '94x94'}
 end
