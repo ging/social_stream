1b98817b616191c9890160ee2ff6f0916816bc54
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-05-09 18:00:19 +0200

    Fix Permission translations

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -1,15 +1,15 @@
 module PermissionsHelper
   DEFAULT_PERMISSIONS =
     [
-      [ "read",    "activity", "star_ties" ],
+      [ "read",    "activity", "weak_star_ties" ],
       [ "create",  "activity", nil ],
-      [ "destroy", "activity", nil ],
       [ "follow", nil, nil]
     ]
 
   def default_permissions
-    DEFAULT_PERMISSIONS.map{ |p|
-      Permission.find_by_action_and_object_and_function *p
-    }
+    @default_permissions ||=
+      DEFAULT_PERMISSIONS.map{ |p|
+        Permission.find_by_action_and_object_and_function *p
+      }
   end
 end
