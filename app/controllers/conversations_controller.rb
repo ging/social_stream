607db59f176cab4a5a8fd8fa7c84b9e63114a088
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-06-10 10:58:33 +0200

    Fix ConversationsController pagination

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,11 +5,11 @@ class ConversationsController < ApplicationController
   before_filter :check_current_subject_in_conversation, :only => [:show, :update, :destroy]
   def index
     if @box.eql?"inbox"
-      @conversations = @mailbox.inbox.page(params[:page]).per(9)
+      @conversations = Kaminari.paginate_array(@mailbox.inbox).page(params[:page]).per(9)
     elsif @box.eql?"sentbox"
-      @conversations = @mailbox.sentbox.page(params[:page]).per(9)
+      @conversations = Kaminari.paginate_array(@mailbox.sentbox).page(params[:page]).per(9)
     else
-      @conversations = @mailbox.trash.page(params[:page]).per(9)
+      @conversations = Kaminari.paginate_array(@mailbox.trash).page(params[:page]).per(9)
     end
   end
 
