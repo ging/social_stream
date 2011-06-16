f9c3ca7ad474d4442670c714c7daaafe18ed758b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-06-16 10:08:17 +0200

    Remove helper loading in Engine

--- lib/social_stream/attachments/engine.rb
+++ lib/social_stream/attachments/engine.rb
@@ -5,10 +5,6 @@ module SocialStream
       initializer "social_stream-attachments.attachment_in_social_stream_objects" do
         SocialStream.objects.push(:attachment) unless SocialStream.objects.include?(:attachment)
       end
-      
-      config.to_prepare do
-        ApplicationController.helper AttachmentsHelper
-      end
     end
   end
 end
