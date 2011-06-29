598d02aa077b9f3fe66585abb5efac3cab8e43f0
Leena <leena.sn@multunus.com>
2011-06-29 12:15:33 +0530

    added modernizr as js asset, modified application layout to include the same, fixed code issues with invitation_controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,17 +5,15 @@ class InvitationsController < ApplicationController
   end
   
   def create
-    puts "You have introduced the following e-mail addresses: "
-    puts params[:mails]
+    logger.debug "You have introduced the following e-mail addresses: #{params[:mails]}"
     
-    puts "And you have added the following message: "
-    puts params[:message]
+    logger.debug "And you have added the following message: #{params[:message]}"
     
     if params[:mails].present?
       InvitationMailer.send_invitation(params[:mails], params[:message]).deliver
-      redirect_to (new_invitation_path, :notice => 'Your invitations have successfully been sent')
+      redirect_to new_invitation_path, :notice => 'Your invitations have successfully been sent'
     else
-      redirect_to (new_invitation_path, :notice => 'Your request was unprocessable')
+      redirect_to new_invitation_path, :notice => 'Your request was unprocessable'
     end
     
   end
