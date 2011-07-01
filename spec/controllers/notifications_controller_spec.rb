a495bf0c42f11c760ef1ea5a93d359ac629fa30f
Alicia Díez <adiezbal@gmail.com>
2011-07-01 13:30:38 +0200

    Notifications fixed and tested

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,47 +1,47 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+
 describe NotificationsController do
   include SocialStream::TestHelpers
   render_views
-  
-  context "for a user" do
-    before do
-      @user = Factory(:user)
-      sign_in @user
-      @receipt = @user.notify("subject", "body")
-    end
-    
-    it "should render index" do
-      get :index
-      assert_response :success
-    end
-    
-    it "should render show" do
-      get :show, :notification_id => @receipt.notification.to_param
-      assert_response :success
-    end
-    
-    it "should update" do
-      put :update, :notification_id => @receipt.notification.to_param
 
-      response.should redirect_to([@user, :profile])
-    end
+  before do
+    @user = Factory(:user)
+    @actor =  @user.actor
+    sign_in @user
+    @receipt = @user.notify("subject", "body", Factory(:activity))
+  end
 
+  it "should render index" do
+    get :index
+    assert_response :success
+  end
+
+  it "should update read" do
+    put :update, :id => @receipt.notification.to_param, :read => "Read"
+    @receipt.notification.is_unread?(@actor).should==false
+    response.should redirect_to notifications_path
+  end
+
+  it "should update unread" do
+    put :update, :id => @receipt.notification.to_param, :read => "Unread"
+    @receipt.notification.is_unread?(@actor).should==true
+    response.should redirect_to notifications_path
+  end
+
+  it "should update all" do
+    @receipt2 = @user.notify("subject", "body", Factory(:activity))
+    put :update_all
+    @receipt.notification.is_unread?(@actor).should==false
+    @receipt2.notification.is_unread?(@actor).should==false
+    response.should redirect_to notifications_path
   end
   
-  context "for a group" do
-    before do
-      membership = Factory(:member)
-      @group = membership.sender_subject
-      @user  = membership.receiver_subject
-
-      sign_in @user
-      represent @group
-    end
+  it "should send to trash" do
+    delete :destroy, :id => @receipt.notification.to_param
+    @receipt.notification.is_trashed?(@actor).should==true
+    response.should redirect_to notifications_path
     
-    it "should render index" do
-      get :index
-      assert_response :success
-    end
   end
+
 end
