4d7010c4c727bc443c40c6f19e4bbac31183323b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-07-04 15:11:00 +0200

    Add documents generator

--- lib/generators/social_stream/install_generator.rb
+++ lib/generators/social_stream/install_generator.rb
@@ -1,3 +1,4 @@
 class SocialStream::InstallGenerator < Rails::Generators::Base #:nodoc:
   hook_for :base
+  hook_for :documents
 end
--- lib/social_stream.rb
+++ lib/social_stream.rb
@@ -4,5 +4,6 @@ require 'social_stream-attachments'
 module SocialStream
   class Engine < ::Rails::Engine #:nodoc:
     config.app_generators.base 'social_stream:base'
+    config.app_generators.documents 'social_stream:documents'
   end
 end
