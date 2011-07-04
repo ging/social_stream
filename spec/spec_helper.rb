e72862005ff088381a7adbe6eded54348f431d62
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-07-04 09:57:52 +0200

    Remove Rspec deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,8 +27,3 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load Factories
 require 'factory_girl'
 Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-  # == Mock Framework
-  config.mock_with :rspec
-end
