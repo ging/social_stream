ab8000604d0a7dd1464405e1f19e2946d98aaaf0
Eduardo Casanova <ecasanovac@gmail.com>
2011-07-05 14:38:19 +0200

    Improved method in activity_controller

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -4,18 +4,8 @@ module ActivitiesHelper
   # @param [Object]
   # @return [String]
   def link_like(object)
-    if !user_signed_in?
-      link_to image_tag("btn/nolike.png", :class => "menu_icon")+
-                t('activity.like'),new_user_session_path
-    else
-      if (object.liked_by?(current_subject))
-        link_to image_tag("btn/like.png", :class => "menu_icon")+
-                t('activity.unlike'), [object, :like], :method => :delete, :remote => true
-      else
-        link_to image_tag("btn/nolike.png", :class => "menu_icon")+
-                t('activity.like'), [object, :like], :method => :post, :remote => true
-      end
-    end
+    params = link_like_params(object)
+    link_to params[0],params[1],params[2]
   end
 
   def link_like_params(object)
