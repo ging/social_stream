4e2b9fab2a4b4f46f73dcadc3d37b2b7d878c50e
Eduardo Casanova <ecasanovac@gmail.com>
2011-07-13 14:06:44 +0200

    Adding peding requests basic page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,16 +3,24 @@ class ContactsController < ApplicationController
   
   def index
     @contacts =
-      current_subject.
-        contact_subjects(:direction => :sent, :relations => params[:relation]){ |q|
-          q.alphabetic.
-            letter(params[:letter]).
-            search(params[:search])
-        }
+      Contact.sent_by(current_subject).
+              joins(:receiver).merge(Actor.alphabetic).
+              merge(Actor.letter(params[:letter])).
+              merge(Actor.search(params[:search]))
+    
+    if params[:pending].present?
+      @contacts = 
+        Contact.received_by(current_subject).
+              joins(:sender).merge(Actor.alphabetic).
+              merge(Actor.letter(params[:letter])).
+              merge(Actor.search(params[:search])).
+              pending.
+              not_reflexive
+    end
 
     respond_to do |format|
-      format.html { @contacts = Kaminari.paginate_array(@contacts).page(params[:page]).per(10) }
-      format.js { @contacts = Kaminari.paginate_array(@contacts).page(params[:page]).per(10) }
+      format.html { @contacts = @contacts.page(params[:page]).per(10) }
+      format.js { @contacts = @contacts.page(params[:page]).per(10) }
       format.json { render :text => @contacts.map{ |c| { 'key' => c.actor_id.to_s, 'value' => self.class.helpers.truncate_name(c.name) } }.to_json }
     end
   end
