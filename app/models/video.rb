bdf0dd0b1221e73fb358aefe11eb9dfaa5988cb0
ebarra <ebarra@pizarro.(none)>
2011-07-19 15:37:45 +0200

    changed has_attached_file param to change the name of the files to the original one

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,7 @@ class Video < Document
   
   has_attached_file :file, 
                     :url => '/:class/:id.:extension',
-                    :path => ':rails_root/documents/:class/:id_partition/:style.:extension',
+                    :path => ':rails_root/documents/:class/:id_partition/:style_:basename.:extension',
                     :styles => {:webm => {:format => 'webm'},
                                 :thumb   => {:geometry => "48x48" , :format => 'png', :time => 5},
                                 :thumb0  => {:geometry => "130x80", :format => 'png', :time => 5}
