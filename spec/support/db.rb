d76974f113422d323caf06008567eed1e1da544c
Eduardo Casanova <ecasanovac@gmail.com>
2011-07-20 11:05:40 +0200

    Solving test db problem with mailboxer

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -1,32 +1,31 @@
 require File.join(File.dirname(__FILE__), 'migrations')
 
+
 begin
   ActsAsTaggableOnMigration.down
 rescue
-  puts "WARNING: ActsAsTaggableOnMigration failed to rollback" 
+  puts "WARNING: ActsAsTaggableOnMigration failed to rollback"
 end
 
-%w(Mailboxer).each do |m|
-  begin
-    "Create#{ m }".constantize.down
-  rescue
-    puts "WARNING: #{ m } migration failed to rollback" 
-  end
+mailboxer_path = Gem::GemPathSearcher.new.find('mailboxer').full_gem_path
+mailboxer_migration = File.join([mailboxer_path,'db', 'migrate'])
+
+begin
+  ActiveRecord::Migrator.migrate mailboxer_migration, 0
+rescue
+  puts "WARNING: Mailboxer migration failed to rollback"
 end
 
 begin
   ActiveRecord::Migrator.migrate File.expand_path("../../dummy/db/migrate/", __FILE__), 0
 rescue
-  puts "WARNING: Social Stream Base failed to rollback" 
+  puts "WARNING: Social Stream Base failed to rollback"
 end
 
-# Mailboxer
-#CreateMailboxer.up
-mailboxer_path = Gem::GemPathSearcher.new.find('mailboxer').full_gem_path
-mailboxer_migration = File.join([mailboxer_path,'db', 'migrate'])
-ActiveRecord::Migrator.migrate mailboxer_migration
 
 ActsAsTaggableOnMigration.up
+
+ActiveRecord::Migrator.migrate mailboxer_migration
 
 # Run any available migration
 ActiveRecord::Migrator.migrate File.expand_path("../../dummy/db/migrate/", __FILE__)
--- spec/support/migrations.rb
+++ spec/support/migrations.rb
@@ -4,7 +4,3 @@ require 'social_stream/migration_finder'
 SocialStream::MigrationFinder.new 'acts-as-taggable-on',
                     ["generators", "acts_as_taggable_on", "migration", "templates", "active_record", "migration"]
 
-# Mailboxer
-mailboxer_path = Gem::GemPathSearcher.new.find('mailboxer').full_gem_path
-mailboxer_migration = File.join([mailboxer_path,'db', 'migrate'])
-ActiveRecord::Migrator.migrate mailboxer_migration
