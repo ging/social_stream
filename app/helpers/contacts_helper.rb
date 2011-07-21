b009aeddb4743426794d3ffeb6b604f7360e966b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-07-21 15:57:05 +0200

    Improve contact helper

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -3,22 +3,26 @@ module ContactsHelper
     "N contacts in common"
   end
 
+  def contact_status(c)
+    c.established? ?
+      c.ties.map(&:relation_name).join(", ") :
+      t("contact.new.link")
+  end
+
+  def contact_link(c)
+    link_to contact_status(c),
+            edit_contact_path(c),
+            :title => t("contact.#{ c.action }.title", :name => c.receiver.name)
+
+  end
+
   # Show current ties from current user to actor, if they exist, or provide a link
   # to create new ties to actor
   def contact_to(a)
     if user_signed_in?
-      link_to contact_status(a),
-              edit_contact_path(current_subject.contact_to!(a)),
-                                :title => t("contact.new.title",
-                                :name => a.name)
+      contact_link current_subject.contact_to!(a)
     else
       link_to t("contact.new.link"), new_user_session_path
     end
-  end
-
-  def contact_status(a)
-    current_subject.ties_to?(a) ?
-      current_subject.ties_to(a).map(&:relation_name).join(", ") :
-      t("contact.new.link")
   end
 end
