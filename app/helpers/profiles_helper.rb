c75782c781dc72a0a356acab3cca381cfdf0ce23
Eduardo Casanova <ecasanovac@gmail.com>
2011-07-21 12:49:56 +0200

    Closes #22

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -28,4 +28,10 @@ module ProfilesHelper
     end
   end
   
+  #Returns the value if user is signed in or a link to sign in view
+  def show_if_signed_in(info)
+    return info if user_signed_in?
+    return link_to t('profile.must_be_signed_id'), new_user_session_path
+  end
+  
 end
