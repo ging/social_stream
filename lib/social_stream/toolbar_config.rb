00978495b675c8b07feda2fdb31d4e2b12389e36
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-07-22 16:05:13 +0200

    Fix toolbar config

--- lib/social_stream/toolbar_config.rb
+++ lib/social_stream/toolbar_config.rb
@@ -29,7 +29,7 @@ module SocialStream
           :url => "#",
           :options => {:link => {:id => "resources_menu"}},
           :items => [
-            {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => documents_path},
+            {:key => :resources_documents,:name => image_tag("btn/btn_document.png")+t('document.title'),:url => documents_path},
             {:key => :resources_pictures,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => pictures_path},
             {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => videos_path},
             {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => audios_path}
@@ -99,10 +99,10 @@ module SocialStream
           :url => "#",
           :options => {:link => {:id => "resources_menu"}},
           :items => [
-            {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => documents_path},
-            {:key => :resources_pictures,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => pictures_path},
-            {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => videos_path},
-            {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => audios_path}
+            {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => polymorphic_path([subject, Document.new])},
+            {:key => :resources_pictures,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => polymorphic_path([subject, Picture.new])},
+            {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => polymorphic_path([subject, Video.new])},
+            {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => polymorphic_path([subject, Audio.new])}
           ]}
       end
       render_items items
