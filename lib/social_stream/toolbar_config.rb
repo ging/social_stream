a85b1308986f18b784790c7d216981532aa295bc
Diego Carrera <diegocarrera2000@gmail.com>
2011-07-26 13:03:13 +0200

    toolbar rename documents

--- lib/social_stream/toolbar_config.rb
+++ lib/social_stream/toolbar_config.rb
@@ -99,7 +99,7 @@ module SocialStream
           :url => "#",
           :options => {:link => {:id => "resources_menu"}},
           :items => [
-            {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => polymorphic_path([subject, Document.new])},
+            {:key => :resources_documents,:name => image_tag("btn/btn_document.png")+t('document.title'),:url => polymorphic_path([subject, Document.new])},
             {:key => :resources_pictures,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => polymorphic_path([subject, Picture.new])},
             {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => polymorphic_path([subject, Video.new])},
             {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => polymorphic_path([subject, Audio.new])}
