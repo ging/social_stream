1db03d61a65516cff4d11f026b49ebc94df5d211
ebarra <ebarra@pizarro.(none)>
2011-07-27 16:21:47 +0200

    small changes in jplayer for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,7 @@ class Video < Document
                     :url => '/:class/:id.:extension',
                     :path => ':rails_root/documents/:class/:id_partition/:style.:extension',
                     :styles => {:webm => {:format => 'webm'},
+                                :flv => { :format => 'flv' }
                                 :thumb   => {:geometry => "48x48" , :format => 'png', :time => 5},
                                 :thumb0  => {:geometry => "130x80", :format => 'png', :time => 5}
                     },:processors => [:ffmpeg]
