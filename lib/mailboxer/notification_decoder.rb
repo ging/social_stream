37d02cb748c62e8999b319f7a32ac2ddedea6492
Karthik Hariharan <kar.hariharan@gmail.com>
2011-08-18 13:33:58 -0500

    Refactored calls in notification_decoder from '.class.to_s.downcase' to '.class.to_s.underscore' to fix errors with multi-word classes being used as Subjects.

--- lib/mailboxer/notification_decoder.rb
+++ lib/mailboxer/notification_decoder.rb
@@ -4,11 +4,11 @@ module Mailboxer
     def decode_notification notification_text, activity
       return if activity.nil?
       notification_text = notification_text.gsub(/\%\{sender\}/, link_to(truncate_name(activity.sender.name),
-                                                                      url_for(:controller=> activity.sender.subject.class.to_s.downcase.pluralize,
+                                                                      url_for(:controller=> activity.sender.subject.class.to_s.underscore.pluralize,
                                                                               :action=> :show, :id=> activity.sender.subject.slug, :only_path => false)))
       notification_text = notification_text.gsub(/\%\{confirm\}/,link_to(t('notification.confirm'),edit_contact_url(activity.receiver.contact_to!(activity.sender), :s => activity.sender.slug)))
       notification_text = notification_text.gsub(/\%\{look\}/,link_to(t('notification.look'),
-                                                                      url_for(:controller=> activity.sender.subject.class.to_s.downcase.pluralize,
+                                                                      url_for(:controller=> activity.sender.subject.class.to_s.underscore.pluralize,
                                                                               :action=> :show, :id=> activity.sender.subject.slug, :only_path => false)))
       notification_text = notification_text.gsub(/\%\{sender.name\}/,truncate_name(activity.sender.name))
 
@@ -23,12 +23,12 @@ module Mailboxer
       if activity.direct_object.present?
         object = activity.direct_object
         object = object.subject if object.is_a? Actor
-        notification_text=notification_text.gsub(/\%\{object\}/,link_to(object.class.to_s.downcase,
-                                                                      url_for(:controller=> object.class.to_s.downcase.pluralize, :action=> :show, 
+        notification_text=notification_text.gsub(/\%\{object\}/,link_to(object.class.to_s.underscore,
+                                                                      url_for(:controller=> object.class.to_s.underscore.pluralize, :action=> :show, 
                                                                               :id=> object.id, :only_path => false)))
-        notification_text=notification_text.gsub(/\%\{object.name\}/,object.class.to_s.downcase)
+        notification_text=notification_text.gsub(/\%\{object.name\}/,object.class.to_s.underscore)
         notification_text=notification_text.gsub(/\%\{object.text\}/,link_to(object.text.truncate(100, :separator =>' '), 
-                                                                          url_for(:controller=> object.class.to_s.downcase.pluralize, :action=> :show, 
+                                                                          url_for(:controller=> object.class.to_s.underscore.pluralize, :action=> :show, 
                                                                                   :id=> object.id, :only_path => false))) if object.respond_to? :text
       
         #notification_text=notification_text.gsub(/\%\{object.image\}/,thumb_for(object)) if SocialStream.activity_forms.include? :document and object.is_a? Document
@@ -59,8 +59,8 @@ module Mailboxer
       if activity.direct_object.present?
         object = activity.direct_object
         object = object.subject if object.is_a? Actor
-        notification_text=notification_text.gsub(/\%\{object\}/,object.class.to_s.downcase)
-        notification_text=notification_text.gsub(/\%\{object.name\}/,object.class.to_s.downcase)
+        notification_text=notification_text.gsub(/\%\{object\}/,object.class.to_s.underscore)
+        notification_text=notification_text.gsub(/\%\{object.name\}/,object.class.to_s.underscore)
         notification_text=notification_text.gsub(/\%\{object.text\}/,object.text.truncate(100, :separator =>' ')) if object.respond_to? :text
       
         #notification_text=notification_text.gsub(/\%\{object.image\}/,thumb_for(object)) if SocialStream.activity_forms.include? :document and object.is_a? Document
