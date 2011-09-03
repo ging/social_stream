d4ef3899e1ff5d9f4ec6de41b6f8b0f0d73419c7
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-09-03 15:27:03 +0200

    Fix spec/dummy schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,12 +1,5 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'support', 'migrations')
+require 'social_stream/migrations/base'
 
 ActiveRecord::Schema.define(:version => 0) do
-  # Mailboxer
-  mailboxer_path = Gem::GemPathSearcher.new.find('mailboxer').full_gem_path
-  mailboxer_migration = File.join([mailboxer_path,'db', 'migrate'])
-  ActiveRecord::Migrator.migrate mailboxer_migration
-  #SocialStream
-  CreateSocialStream.up
-  #ActAsTaggable
-  ActsAsTaggableOnMigration.up
+  SocialStream::Migrations::Base.new.up
 end
