17637ae8591407ed11568c645b459ce5fda7ca46
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-09-14 16:33:05 +0200

    Show message when graph is empty. Closes #58

--- app/controllers/ties_controller.rb
+++ app/controllers/ties_controller.rb
@@ -4,5 +4,8 @@ class TiesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    unless current_subject.sent_contacts.active.count > 0
+      flash[:notice] = t('contact.graph.empty')
+    end
   end
 end
