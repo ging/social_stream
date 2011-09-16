32746539d311a17ae8796efe9c1917c1198c8577
Eduardo Casanova <ecasanovac@gmail.com>
2011-09-16 13:52:01 +0200

    When activity object is a subject/actor renders _with_details partial

--- base/app/helpers/search_helper.rb
+++ base/app/helpers/search_helper.rb
@@ -1,5 +1,6 @@
 module SearchHelper  
   def subject_with_details subject
+    subject = subject.subject if subject.is_a? Actor    
     render :partial => subject.class.to_s.pluralize.downcase + '/' + subject.class.to_s.downcase + '_with_details',
            :locals => {subject.class.to_s.downcase.to_sym => subject}
   end
