e638f94bba80031e8e90fd64afedd320224948d4
Eduardo Casanova <ecasanovac@gmail.com>
2011-09-16 13:19:08 +0200

    Improved search visuals

--- base/app/controllers/search_controller.rb
+++ base/app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
   
   def index
     if params[:mode].eql? "header_search"
-      @search_result = ThinkingSphinx.search "*#{params[:search_query]}*", :page => 1, :per_page => 10, :classes => SocialStream.subjects.map{|sym| sym = sym.to_s.classify.constantize}
+      @search_result = header_search params[:search_query]
       render :partial => "header_search", :locals => {:search_result => @search_result}
       return
     else
@@ -16,11 +16,20 @@ class SearchController < ApplicationController
 
   private
   def global_search query
+    return search query, params[:page].present? ? params[:page] : 1, 10
+  end
+  
+  def header_search query
+    return search query, params[:page].present? ? params[:page] : 1, 3
+  end
+  
+  def search query, page, per_page
     result = Hash.new
     SocialStream.subjects.each do |subject_sym|
-      result.update({subject_sym => ThinkingSphinx.search("*#{query}*", :page => 1, :per_page => 10, :classes => [subject_sym.to_s.classify.constantize])})
+      result.update({subject_sym => ThinkingSphinx.search("*#{query}*", :page => page, :per_page => per_page, :classes => [subject_sym.to_s.classify.constantize])})
       result.update({(subject_sym.to_s+"_total").to_sym => ThinkingSphinx.count("*#{query}*", :classes => [subject_sym.to_s.classify.constantize])})
     end
     return result
+    
   end
 end
