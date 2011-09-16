93399dc9e36009a2342de4e74e0f811b0f8a8d61
Eduardo Casanova <ecasanovac@gmail.com>
2011-09-16 11:49:05 +0200

    Added none and search all comment for global search

--- base/app/controllers/search_controller.rb
+++ base/app/controllers/search_controller.rb
@@ -19,6 +19,7 @@ class SearchController < ApplicationController
     result = Hash.new
     SocialStream.subjects.each do |subject_sym|
       result.update({subject_sym => ThinkingSphinx.search("*#{query}*", :page => 1, :per_page => 10, :classes => [subject_sym.to_s.classify.constantize])})
+      result.update({(subject_sym.to_s+"_total").to_sym => ThinkingSphinx.count("*#{query}*", :classes => [subject_sym.to_s.classify.constantize])})
     end
     return result
   end
