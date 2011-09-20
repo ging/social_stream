7a50a3ceb7fc51ac4339037b14d294fe4c14ce8a
Eduardo Casanova <ecasanovac@gmail.com>
2011-09-20 23:10:06 +0200

    Added error notification when search query is too short

--- base/app/helpers/search_helper.rb
+++ base/app/helpers/search_helper.rb
@@ -9,4 +9,10 @@ module SearchHelper
     search_class = search_class.to_s if search_class.is_a? Class or search_class.is_a? Symbol
     link_to text, search_path(:focus => search_class.downcase.pluralize, :search_query => query ), :remote => true
   end
+  
+  def too_short_query?
+    return true if params[:search_query].blank?
+    bare_query = strip_tags(params[:search_query]) unless bare_query.html_safe?
+    return bare_query.strip.size < SearchController::MIN_QUERY
+  end
 end
