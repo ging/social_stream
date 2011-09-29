2bd612223c2ffc9cab7e1de06ced6ca921b59371
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-09-29 16:55:24 +0200

    Remove conference_manager, as it is already packaged

--- events/app/models/conference_manager/start.rb
+++
@@ -1,9 +0,0 @@
-module ConferenceManager
-  class Start < Resource
-    singleton
-
-    self.element_name = "restart" 
-    self.site = domain
-    self.prefix = "/events/:event_id/"
-  end   
-end
