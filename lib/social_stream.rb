14d7088c10e721d82ac772c5a5142594803b06ab
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-09-29 16:22:27 +0200

    Modify activity_form order

--- lib/social_stream.rb
+++ lib/social_stream.rb
@@ -3,6 +3,10 @@ require 'social_stream-documents'
 require 'social_stream-events'
 
 module SocialStream
+  # Put :group at the end of activity_forms
+  activity_forms.delete(:group)
+  activity_forms.push(:group)
+
   class Engine < ::Rails::Engine #:nodoc:
     config.app_generators.base      'social_stream:base'
     config.app_generators.documents 'social_stream:documents'
