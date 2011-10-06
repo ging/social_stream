abf214bb6426d7d65a86ef1655e600fc6de0b529
Eduardo Casanova <ecasanovac@gmail.com>
2011-10-06 13:27:09 +0200

    Working towards the new search behaviour

--- base/app/controllers/search_controller.rb
+++ base/app/controllers/search_controller.rb
@@ -1,23 +1,24 @@
 class SearchController < ApplicationController
   include ActionView::Helpers::SanitizeHelper
+  
+  helper_method :get_search_query
 
   FOCUS_SEARCH_PER_PAGE=24
   MIN_QUERY=2
   
   def index
-    @search_class_sym = params[:focus].singularize.to_sym unless params[:focus].blank?
     if params[:search_query].blank? or too_short_query
-      @search_result = nil_search
+    @search_result = []
     else
       if params[:mode].eql? "header_search"
-        @search_result = header_search
-        render :partial => "header_search", :locals => {:search_result => @search_result}
+        @search_result = search :quick
+        render :partial => "header_search"
       return
       else
         if params[:focus].present?
           @search_result = focus_search
         else
-          @search_result = global_search
+          @search_result = search :extended
         end
       end
     end
@@ -28,6 +29,25 @@ class SearchController < ApplicationController
   end
 
   private
+
+  def search mode
+    models = SocialStream.extended_search_models
+    models = SocialStream.quick_search_models if mode.to_s.eql? "quick"
+    models.map! {|model_sym| model_sym.to_s.classify.constantize}
+    result = ThinkingSphinx.search(get_search_query, :classes => models)
+    if mode.to_s.eql? "quick"
+      result.page(1).per(7)
+    else
+      result.page(params[:page]).per(FOCUS_SEARCH_PER_PAGE)
+    end
+    return result
+  end
+
+  def focus_search
+    @search_class_sym = params[:focus].singularize.to_sym unless params[:focus].blank?
+    search_class = @search_class_sym.to_s.classify.constantize
+    ThinkingSphinx.search(get_search_query, :classes => [search_class]).page(params[:page]).per(FOCUS_SEARCH_PER_PAGE)
+  end
 
   def too_short_query
     bare_query = strip_tags(params[:search_query]) unless bare_query.html_safe?
@@ -43,45 +63,5 @@ class SearchController < ApplicationController
       search_query+= "*" + search_query_words[i] + "* " if i == (search_query_words.size - 1)
     end
     return search_query.strip
-  end
-
-  def global_search
-    return search "extended", 5
-  end
-
-  def header_search
-    return search "quick", 2
-  end
-
-  def search mode, max_results
-    result = Hash.new
-    models = SocialStream.extended_search_models
-    models = SocialStream.quick_search_models if mode.eql? "quick"
-    total_shown = 0
-    models.each do |model_sym|
-      result.update({model_sym => ThinkingSphinx.search(get_search_query, :classes => [model_sym.to_s.classify.constantize]).page(1).per(max_results)})
-      result.update({(model_sym.to_s+"_total").to_sym => ThinkingSphinx.count(get_search_query, :classes => [model_sym.to_s.classify.constantize])})
-    end
-    return result
-  end
-
-  def focus_search
-    string_class = params[:focus].singularize
-    search_class = string_class.classify.constantize
-    return ThinkingSphinx.search(get_search_query, :classes => [search_class]).page(params[:page]).per(FOCUS_SEARCH_PER_PAGE)
-  end
-
-  def nil_search
-    if params[:focus].present?
-    result = []
-    else
-      models = SocialStream.extended_search_models | SocialStream.quick_search_models
-      result = Hash.new
-      models.each do |model_sym|
-        result.update({model_sym => []})
-        result.update({(model_sym.to_s+"_total").to_sym => 0})
-      end
-    end
-    return result
   end
 end
