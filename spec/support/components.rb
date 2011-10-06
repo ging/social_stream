f38da078676bb92eeb76fb4d0c873d04032e0d7a
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-10-06 16:13:04 +0200

    Fixing the thinking-sphinx sqlite3 issue

--- spec/support/components.rb
+++ spec/support/components.rb
@@ -1,5 +1,5 @@
 {
-  :base => [ :cancan, :devise, :mock ]
+  :base => [ :cancan, :devise, :mock, 'thinking-sphinx' ]
 }.each_pair do |component, files|
   files.each do |file|
     require File.expand_path("../../../#{ component }/spec/support/#{ file }.rb", __FILE__)
