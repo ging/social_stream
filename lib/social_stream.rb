0e2d54a7b761d63042225e3449a9e4b8fa492699
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-10-11 10:31:09 +0200

    Restore group menu entry

--- lib/social_stream.rb
+++ lib/social_stream.rb
@@ -3,10 +3,6 @@ require 'social_stream-documents'
 require 'social_stream-events'
 
 module SocialStream
-  # Put :group at the end of activity_forms
-  activity_forms.delete(:group)
-  activity_forms.push(:group)
-
   class Engine < ::Rails::Engine #:nodoc:
     config.app_generators.base      'social_stream:base'
     config.app_generators.documents 'social_stream:documents'
