377fb7718f73935f7a504edd8d5f58e6016da400
Eduardo Casanova <ecasanovac@gmail.com>
2011-10-13 17:30:03 +0200

    Merging Edit User and Settings. Fixes #121

--- base/app/assets/javascripts/settings.js
+++ base/app/assets/javascripts/settings.js
@@ -1,35 +1,14 @@
-var notifications_open = false;
-var api_key_open = false;
-
 closeAllSettings = function() {
-    $("#notifications_settings_content").hide("slow");
-    $("#notifications_settings_briefing").show("slow");
-    notifications_open = false;
-    $("#api_key_settings_content").hide("slow");
-    $("#api_key_settings_briefing").show("slow");
-    api_key_open = false;
+  $(".settings_content").hide("slow");
+  $(".settings_briefing").show("slow");
 }
-showNotificationsSettings = function() {
-    if (notifications_open) {
-        $("#notifications_settings_content").hide("slow");
-        $("#notifications_settings_briefing").show("slow");
-        notifications_open = false;
-    } else {
-        closeAllSettings();
-        $("#notifications_settings_content").show("slow");
-        $("#notifications_settings_briefing").hide("slow");
-        notifications_open = true;
-    }
-}
-showApiKeySettings = function() {
-    if (api_key_open) {
-        $("#api_key_settings_content").hide("slow");
-        $("#api_key_settings_briefing").show("slow");
-        api_key_open = false;
-    } else {
-        closeAllSettings();
-        $("#api_key_settings_content").show("slow");
-        $("#api_key_settings_briefing").hide("slow");
-        api_key_open = true;
-    }
+showSettings = function(name) {
+  closeAllSettings();
+  if($("#" + name + "_content").css("display") == "none") {
+    $("#" + name + "_briefing").hide("slow");
+    $("#" + name + "_content").show("slow");
+  } else {
+    $("#" + name + "_briefing").show("slow");
+    $("#" + name + "_content").hide("slow");
+  }
 }
--- base/app/controllers/settings_controller.rb
+++ base/app/controllers/settings_controller.rb
@@ -1,5 +1,5 @@
 class SettingsController < ApplicationController
-  
+    
   before_filter :authenticate_user!
   
   def index
