8ce05c8b6ada02e1ed9cb6c93ffa8ea64f75080a
Aldo Gordillo <iamchrono@gmail.com>
2011-10-24 12:30:22 +0200

    Initializer changes

--- presence/lib/generators/social_stream/presence/templates/initializer.rb
+++ presence/lib/generators/social_stream/presence/templates/initializer.rb
@@ -2,7 +2,7 @@ SocialStream::Presence.setup do |config|
   #Configures XMPP Server Domain
   config.domain = "localhost"
   #Configures Bosh Service Path
-  config.bosh_service = "http://localhost:8080/http-bind"
+  #config.bosh_service = "http://xmpp-proxy/http-bind"
   #Configures Social Stream Rails App Password
   config.password = "DnVCB8G|R$VGmZ@2?5=CYS8z)NrL@LuQ&LTCUh^9B(DF4gC&sQpfbCMbaNKEdNrGYkT4L5zxM0wNWs5q3?ww(b&0d5fK87z^BmgJMMF2SKXT9pEk^UEcch!GX!Avf5GT9)j@FpHe)4RH)BK7J98u!sUmJHUN(Je6aBmn!FtZ4Ab5h8$|nsvUt3Jkq?21HOH$r0sDyJZZvGOCgFS2EKw@0wXsJRHYVRPBe&Eb!1X55e55bQ^h2AW&^R70TK0m)Fu8"
   #Configures XMPP Server Password
--- presence/lib/social_stream/presence/version.rb
+++ presence/lib/social_stream/presence/version.rb
@@ -1,5 +1,5 @@
 module Socialstream
   module Presence
-    VERSION = "0.0.6"
+    VERSION = "0.0.7"
   end
 end
