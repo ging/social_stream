13d3838fc6421445ec5454e1da4277eb3e4c2713
Aldo Gordillo <iamchrono@gmail.com>
2011-10-25 15:02:20 +0200

    Ejabberd configuration files reorganized

--- presence/ejabberd/ejabberd_scripts/development_scripts/reset_logs.sh
+++ presence/ejabberd/ejabberd_scripts/development_scripts/reset_logs.sh
@@ -3,14 +3,12 @@
 echo "Starting Reset logs"
 
 rm /var/log/ejabberd/auth.log
-rm /var/log/ejabberd/auth_error.log
 rm /var/log/ejabberd/ejabberd.log
 rm /var/log/ejabberd/erlang.log
 rm /var/log/ejabberd/scripts.log
 rm /var/log/ejabberd/erl_crash_*
 
 touch /var/log/ejabberd/auth.log
-touch /var/log/ejabberd/auth_error.log
 touch /var/log/ejabberd/ejabberd.log
 touch /var/log/ejabberd/erlang.log
 touch /var/log/ejabberd/scripts.log
