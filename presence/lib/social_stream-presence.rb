98e82bb38d990f326679e3f982c752d15e850d9f
Aldo Gordillo <iamchrono@gmail.com>
2011-11-02 13:19:01 +0100

    Authentication by cookie implemented

--- presence/lib/social_stream-presence.rb
+++ presence/lib/social_stream-presence.rb
@@ -9,19 +9,21 @@ module SocialStream
       autoload :BuddyManager, 'social_stream/presence/models/buddy_manager'
     end
 
+    mattr_accessor :domain
     mattr_accessor :bosh_service
+    mattr_accessor :auth_method
     mattr_accessor :xmpp_server_password
     mattr_accessor :remote_xmpp_server
     mattr_accessor :enable
     
-    mattr_accessor :domain
     mattr_accessor :social_stream_presence_username
     mattr_accessor :password
 
     mattr_accessor :scripts_path
 
-    @@enable = true
+    @@auth_method = "cookie"
     @@remote_xmpp_server = false
+    @@enable = true
     
     class << self
       def setup
--- presence/lib/social_stream/presence/version.rb
+++ presence/lib/social_stream/presence/version.rb
@@ -1,5 +1,5 @@
 module Socialstream
   module Presence
-    VERSION = "0.0.14"
+    VERSION = "0.0.15"
   end
 end
