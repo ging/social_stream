b0fb9cc63e8ac07760edea7ca65a299912f04a6a
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-11-21 08:32:21 +0100

    0.12.14

--- base/lib/social_stream/base/version.rb
+++ base/lib/social_stream/base/version.rb
@@ -1,5 +1,5 @@
 module SocialStream
   module Base
-    VERSION = "0.9.35".freeze
+    VERSION = "0.9.36".freeze
   end
 end
--- documents/lib/social_stream/documents/version.rb
+++ documents/lib/social_stream/documents/version.rb
@@ -1,5 +1,5 @@
 module SocialStream
   module Documents
-    VERSION = "0.4.7".freeze
+    VERSION = "0.4.8".freeze
   end
 end
