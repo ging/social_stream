cd06026a2e186db5012f2d14c12f5d7e3a103c49
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-12-01 16:32:10 +0100

    Fix comments

--- base/app/controllers/comments_controller.rb
+++ base/app/controllers/comments_controller.rb
@@ -1,18 +1,8 @@
-class CommentsController < InheritedResources::Base
-  load_and_authorize_resource
+class CommentsController < ApplicationController
+  include SocialStream::Controllers::Objects
 
-  respond_to :html, :xml, :js
-
-  def destroy
-    @post_activity = resource.post_activity
-
-    destroy!
-  end
-  
   def show    
     parent = resource.post_activity.parent
     redirect_to polymorphic_path(parent.direct_object,:anchor => dom_id(parent))
   end
-
-
 end
