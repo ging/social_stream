a69b5fc2b4407d7935cac12b6a8542306ac2ffe0
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-12-09 14:35:46 +0100

    Clean events towards next approach

--- events/app/assets/javascripts/social_stream-events.js
+++ events/app/assets/javascripts/social_stream-events.js
@@ -1,5 +1,1 @@
-//= require fullcalendar
-//= require gcal
-//= require jquery.ad-gallery
-//= require jquery.ad-gallery.pack
 //= require_tree .
--- events/lib/social_stream-events.rb
+++ events/lib/social_stream-events.rb
@@ -1,25 +1,7 @@
 require 'social_stream-base'
-require 'conference_manager-ruby'
 
 module SocialStream
-  module ToolbarConfig
-    autoload :Events, 'social_stream/toolbar_config/events'
-  end
-
   module Events
-    # Add :event, :agenda, :session to SocialStream's subjects, objects and activity_forms
-    # by default
-    # It can be configured by users at application's config/initializers/social_stream.rb
-    SocialStream.subjects.push(:event) unless SocialStream.subjects.include?(:event)
-    
-    SocialStream.quick_search_models.push(:event) unless SocialStream.quick_search_models.include?(:event)
-    SocialStream.extended_search_models.push(:event) unless SocialStream.extended_search_models.include?(:event)
-
-    [ :agenda, :session ].each do |o|
-      SocialStream.objects.push(o) unless SocialStream.objects.include?(o)
-    end
-    
-    SocialStream.activity_forms.push(:event) unless SocialStream.activity_forms.include?(:event)
   end
 end
 
