f03663d70d32204f4c22f924f3d237cb964e4e9f
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-01-03 15:39:47 +0100

    Disable update activity

--- base/lib/social_stream/models/object.rb
+++ base/lib/social_stream/models/object.rb
@@ -21,7 +21,9 @@ module SocialStream
           validates_presence_of :author_id, :owner_id, :user_author_id
 
           after_create :create_post_activity
-          after_update :create_update_activity
+          # Disable update activity for now
+          # It usually appears repeated in the wall and provides no useful information
+          #after_update :create_update_activity
         end
       end
 
