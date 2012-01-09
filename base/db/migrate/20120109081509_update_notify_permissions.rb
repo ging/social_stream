4d491cbdf8173794b9ef046a291b33b277005d2a
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-01-09 15:56:00 +0100

    Rubyfy commits e2ba584 and 6021a6e

--- base/db/migrate/20120109081509_update_notify_permissions.rb
+++ base/db/migrate/20120109081509_update_notify_permissions.rb
@@ -2,31 +2,12 @@
 # This migration preserves the old behavior by granting the 'notify' permission to the first relation in each group
 class UpdateNotifyPermissions < ActiveRecord::Migration
   def up
-    # Save record_timestamp, set to false
-    r_ts = RelationPermission.record_timestamps
-    RelationPermission.record_timestamps = false
-
-    # Make sure 'notify' exists
-    perm_notify = Permission.where(:action => 'notify')[0]
-    if perm_notify.nil?
-      perm_notify = Permission.create(:action => 'notify')
+    perm_notify = Permission.find_or_create_by_action('notify')
+    Group.all.each do |g|
+      r = g.relation_customs.first
+      next if r.blank?
+      r.permissions << perm_notify
     end
-
-    seen_actors=[]
-    Relation.where(:sender_type => 'Group', :type => 'Relation::Custom').each do |r|
-      next if seen_actors.include? r.actor
-      seen_actors << r.actor
-      # INSERT INTO permission_relations
-      RelationPermission.create do |rp|
-        rp.relation = r
-	rp.permission = perm_notify
-	rp.created_at = r.created_at
-	rp.updated_at = r.updated_at
-      end
-    end
-
-    # Recover record_timestamps
-    RelationPermission.record_timestamps = r_ts
   end
 
   def down
