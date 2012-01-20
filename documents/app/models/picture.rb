a90bf94025490d8911e4fcdb7cb46563ec046b4e
Raquel García <raquel.garcia.bautista@gmail.com>
2012-01-20 14:02:30 +0100

    Unify wall thumbnail width to 130 pixels, matching linkser

--- documents/app/models/picture.rb
+++ documents/app/models/picture.rb
@@ -2,9 +2,9 @@ class Picture < Document
   has_attached_file :file, 
                     :url => '/:class/:id.:extension',
                     :path => ':rails_root/documents/:class/:id_partition/:style',
-                    :styles => {:thumb  => ["48>"],
-                                :thumb0 => ["130x80"],
-                                :thumb1 => ["500>"]
+                    :styles => {:thumb48sq  => ["48x48"],
+                                :thumbwall => ["130x97#"],
+                                :preview => ["500>"]
                                }                              
                                
   define_index do
@@ -22,11 +22,11 @@ class Picture < Document
       when 16
         "#{ size.to_s }/photo.png"
       when 48
-        helper.picture_path self, :format => format, :style => 'thumb'   
+        helper.picture_path self, :format => format, :style => 'thumb48sq'
       when 130
-        helper.picture_path self, :format => format, :style => 'thumb0'
+        helper.picture_path self, :format => format, :style => 'thumbwall'
       when 500
-        helper.picture_path self, :format => format, :style => 'thumb1'
+        helper.picture_path self, :format => format, :style => 'preview'
     end
   end
       
