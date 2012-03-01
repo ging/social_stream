3f32d0261a3a27066ed136871accf1ce0ff8f2c3
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-03-01 11:06:07 +0100

    Clean search view

--- base/app/helpers/search_helper.rb
+++ base/app/helpers/search_helper.rb
@@ -1,10 +1,4 @@
 module SearchHelper  
-  
-  def focus_search_link text, search_class, query
-    search_class = search_class.to_s if search_class.is_a? Class or search_class.is_a? Symbol
-    link_to text, search_path(:focus => search_class.downcase.pluralize, :search_query => query ), :remote => true
-  end
-  
   def too_short_query?
     return true if params[:search_query].blank?
     bare_query = strip_tags(params[:search_query]) unless bare_query.html_safe?
@@ -34,5 +28,41 @@ module SearchHelper
     search_query = ""
     bare_query = strip_tags(params[:search_query]) unless bare_query.html_safe?
     return bare_query.strip.split
+  end
+
+  def search_class(type, model_sym)
+    case type
+    when :selected
+     params[:focus].present? &&
+      params[:focus].eql?(model_sym.to_s) &&
+      'selected' || ''
+    when :disabled
+      search_results?(model_sym) &&
+        '' || 'disabled'
+    else
+      raise "Unknown select search class type"
+   end
+  end
+
+  def search_results?(model_sym)
+    ThinkingSphinx.count(get_search_query,
+                         :classes => [model_sym.to_s.classify.constantize]) > 0
+  end
+
+  def search_tab(model_sym)
+    span_options = {}
+    span_options[:class] = "#{ search_class(:selected, model_sym) } #{ search_class(:disabled, model_sym) } #{ model_sym.to_s.pluralize.downcase }"
+
+    results = search_results?(model_sym)
+
+    unless results
+      span_options[:title] = t('search.no_subject_found', :subject => model_sym.to_s)
+    end
+
+    link_to_if results,
+               content_tag(:span, t("#{ model_sym }.title.other"), span_options),
+               search_path(:focus => model_sym,
+                           :search_query => params[:search_query]),
+               :remote => true
   end
 end
