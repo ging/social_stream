35edef229ea8f2c8b341cfe75aac40bd368a575d
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-03-06 15:56:17 +0100

    Fix event_timeline_thumb

--- events/app/helpers/events_helper.rb
+++ events/app/helpers/events_helper.rb
@@ -1,6 +1,8 @@
 module EventsHelper
   def event_timeline_thumb(event)
-    if event.poster.present?
+    if event.poster.new_record?
+      ""
+    else
       thumb_for(event.poster, 80)
     end
   end
