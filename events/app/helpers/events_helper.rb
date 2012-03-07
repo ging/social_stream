337a04259c5ef37c509bb3a0ee5f3747b3505d9b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-03-07 09:09:29 +0100

    Default poster image

--- events/app/helpers/events_helper.rb
+++ events/app/helpers/events_helper.rb
@@ -1,7 +1,7 @@
 module EventsHelper
   def event_timeline_thumb(event)
     if event.poster.new_record?
-      ""
+      image_tag("poster.png")
     else
       thumb_for(event.poster, 80)
     end
