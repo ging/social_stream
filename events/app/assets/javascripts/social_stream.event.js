a658ba2cc0e1ba9682efc82a939f076ba032df75
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-03-18 12:22:53 +0100

    Refactor events/event

--- events/app/assets/javascripts/social_stream.event.js
+++ events/app/assets/javascripts/social_stream.event.js
@@ -1,3 +1,5 @@
+//= require social_stream.timeline
+
 SocialStream.Event = (function(SS, $, undefined) {
 	var indexCallbacks = [];
 
@@ -8,6 +10,28 @@ SocialStream.Event = (function(SS, $, undefined) {
 	var index = function(){
 		$.each(indexCallbacks, function(i, callback){ callback(); });
 	}
+
+        var fixDates = function(){
+          $(".event").each(function(){
+            dateString = $(this).find("time").attr("datetime");
+
+            date = new Date(dateString);
+
+            $(this).find(".event_day").text($.datepicker.formatDate('d', date));
+            $(this).find(".event_month").text($.datepicker.formatDate('M', date));
+
+            var hour = $(this).find(".event_hour");
+            if(hour && hour.length) {
+              var minutes = date.getMinutes();
+              if (minutes<10) minutes = "0" + minutes;
+              hour.text(date.getHours() + ':' + minutes);
+            }
+
+          });
+
+        }
+
+        SocialStream.Timeline.addInitCallback(fixDates);
 
 	return {
 		addIndexCallback: addIndexCallback,
