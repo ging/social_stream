2399459403de3ccac4609d29cc3e2a1f47360f94
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-03-28 09:02:17 +0200

    Avoid overwriting after_sign_in_path_for but use Devise's stored_location_for

--- base/lib/social_stream/controllers/cancan_devise_integration.rb
+++ base/lib/social_stream/controllers/cancan_devise_integration.rb
@@ -4,14 +4,6 @@ module SocialStream
     module CancanDeviseIntegration
       extend ActiveSupport::Concern
 
-      included do
-        def after_sign_in_path_for(resource)
-          returning_to = request.env['omniauth.origin'] || stored_location_for(resource) || session[:return_to] || root_path
-          session[:return_to] = nil
-          returning_to
-        end
-      end
-
       private
 
       # Catch some authorization errors:
@@ -25,15 +17,18 @@ module SocialStream
         if user_signed_in?
           if params[:s].present? && controller_name != 'home'
             redirect_to :home
-          else
-            raise exception
+            return
           end
         else
-          session[:return_to] = request.fullpath
-          redirect_to new_user_session_path
+          if request.get?
+            session["user_return_to"] = request.fullpath
+            redirect_to new_user_session_path
+            return
+          end
         end
+
+        raise exception
       end
     end
   end
 end
-
