9a0b35e0128b7b6e2e563c4f044661dd38b65c2a
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-04-19 17:58:19 +0200

    Fix migration

--- base/db/migrate/20120403175913_create_activity_object_audiences.rb
+++ base/db/migrate/20120403175913_create_activity_object_audiences.rb
@@ -1,3 +1,4 @@
+# This migration comes from social_stream_base_engine (originally 20120403175913)
 class CreateActivityObjectAudiences < ActiveRecord::Migration
   def change
     create_table :activity_object_audiences do |t|
@@ -10,13 +11,17 @@ class CreateActivityObjectAudiences < ActiveRecord::Migration
     add_foreign_key :activity_object_audiences, :activity_objects, :name => 'activity_object_audiences_on_activity_object_id'
     add_foreign_key :activity_object_audiences, :relations, :name => 'activity_object_audiences_on_relation_id'
 
-    ActivityObjectAudience.record_timestamps = false
 
     ActivityObject.all.each do |ao|
       post_activity = ao.post_activity
       next if post_activity.blank?
 
       ao.relation_ids = post_activity.relation_ids
+    end
+
+    ActivityObjectAudience.record_timestamps = false
+
+    ActivityObject.all.each do |ao|
       ao.activity_object_audiences.each do |aud|
         aud.created_at = aud.updated_at = ao.created_at
         aud.save!
