c5fb4564f2345c2011412714a7facdee0e9dc338
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-05-09 16:26:20 +0200

    Temporary fix for events#index

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -29,7 +29,7 @@ class EventsController < ApplicationController
 
   def collection
     @activities =
-      profile_subject.wall(:profile,
+      (profile_subject || current_subject).wall(:profile,
                            :for => current_subject,
                            :object_type => :Event)
   end
