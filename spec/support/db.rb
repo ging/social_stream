863dcea2d2ca6d7f8ddcd85d5e9ed77f5e9c85e8
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-06-27 14:21:48 +0200

    Try another way to reset db schema in models

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -18,4 +18,4 @@ end
 #
 # Some models are loaded before the database is created,
 # reporting their table does not exist in specs
-ActiveRecord::Base.connection.schema_cache.clear!
+ActiveRecord::Base.descendants.map(&:reset_column_information)
