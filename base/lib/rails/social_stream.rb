a7b22b95db47b4aa0205ff853e1cfada9abd9cfb
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-08-01 12:36:05 +0200

    Patch `which file` command in windows
    It returns nil. Should provide a better solution

--- base/lib/rails/social_stream.rb
+++ base/lib/rails/social_stream.rb
@@ -5,7 +5,7 @@ class ActionDispatch::Http::UploadedFile
   def initialize_with_magic(*args, &block)
     initialize_without_magic(*args, &block)
 
-    if (unix_file = `which file`.chomp).present? && File.exists?(unix_file)
+    if (unix_file = `which file`.try(:chomp)).present? && File.exists?(unix_file)
       `#{ unix_file } -v 2>&1` =~ /^file-(.*)$/
       version = $1
 
