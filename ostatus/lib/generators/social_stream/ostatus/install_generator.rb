6fc22bcc3be5503247875314024fdf808cccad66
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-09-05 14:57:24 +0200

    Update migration

--- ostatus/lib/generators/social_stream/ostatus/install_generator.rb
+++ ostatus/lib/generators/social_stream/ostatus/install_generator.rb
@@ -2,7 +2,14 @@ class SocialStream::Ostatus::InstallGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   
   source_root File.expand_path('../templates', __FILE__)
-  
+
+   def create_migration_file
+    require 'rake'
+    Rails.application.load_tasks
+    Rake::Task['railties:install:migrations'].reenable
+    Rake::Task['social_stream_ostatus_engine:install:migrations'].invoke
+  end
+ 
   def config_initializer
     copy_file 'initializer.rb', 'config/initializers/social_stream-ostatus.rb'
   end
@@ -11,15 +18,5 @@ class SocialStream::Ostatus::InstallGenerator < Rails::Generators::Base
     append_file 'config/relations.yml',
                      "\nremote_subject:\n  friend:\n    name: friend\n    permissions:\n      - [ follow ]\n    sphere: personal\n"+
                                         "  public:\n    name: public\n    permissions:\n      - [ read, tie, star_tie ]\n    sphere: personal"  
-  end
-
-  require 'rails/generators/active_record'
-
-  def self.next_migration_number(dirname)
-    ActiveRecord::Generators::Base.next_migration_number(dirname)
-  end
-
-  def create_migration_file
-    migration_template 'migration.rb', 'db/migrate/create_social_stream_ostatus.rb'
   end
 end
---
+++ ostatus/lib/social_stream/migrations/ostatus.rb
@@ -0,0 +1,9 @@
+require 'social_stream/migrations/components'
+
+module SocialStream
+  module Migrations
+    # Migrate Ostatus
+    class Ostatus < Components
+    end
+  end
+end
