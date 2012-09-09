8f8320bc94366819a69cded74f9aa1e1bdbdedc7
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-09-09 17:53:08 +0200

    Add http schema to default hub

--- ostatus/lib/generators/social_stream/ostatus/templates/initializer.rb
+++ ostatus/lib/generators/social_stream/ostatus/templates/initializer.rb
@@ -1,6 +1,6 @@
 SocialStream::Ostatus.setup do |config|
   # Default to the PubSubHubbub reference Hub server
-  # config.hub = 'pubsubhubbub.appspot.com'
+  # config.hub = 'http://pubsubhubbub.appspot.com'
 
   config.node_base_url = 'http://localhost:3000'
 end
