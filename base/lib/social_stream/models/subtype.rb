1ffea94bd5021145e3730fbce5c3857b14471ce4
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-09-09 23:25:08 +0200

    Initialize supertype's subtype

--- base/lib/social_stream/models/subtype.rb
+++ base/lib/social_stream/models/subtype.rb
@@ -22,9 +22,11 @@ module SocialStream #:nodoc:
 
         class_eval <<-EOS
           def #{ supertype_name }!                                              # def actor!
-            #{ supertype_name } ||                                              #   actor ||
-              # FIXME: ruby1.9 remove .inspect
-              build_#{ supertype_name }(#{ supertype_options[:build].inspect }) #     build_actor(:subject_type => "User")
+            return #{ supertype_name } if #{ supertype_name }.present?          #   return actor if actor.present?
+            # FIXME: ruby1.9 remove .inspect
+            @_supertype =  build_#{ supertype_name }(#{ supertype_options[:build].inspect }) #     build_actor(:subject_type => "User")
+            @_supertype.#{ base_class.name.underscore } = self
+            @_supertype
           end                                                                   # end
 
         EOS
