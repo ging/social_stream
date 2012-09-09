915f829248ea6b9245ce7697e07525f2ebb15abd
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-09-09 23:31:39 +0200

    Add subject and alias to webfinger info

--- ostatus/app/controllers/webfinger_controller.rb
+++ ostatus/app/controllers/webfinger_controller.rb
@@ -5,9 +5,13 @@ class WebfingerController < ActionController::Metal
   def index
     actor = Actor.find_by_webfinger!(params[:q])
 
-    finger = Proudhon::Finger.new :links => {
-      :profile => polymorphic_url([actor.subject, :profile])
-    }
+    finger = Proudhon::Finger.new(
+      :subject => "acct:#{ actor.slug }@#{ request.host_with_port }",
+      :alias   => polymorphic_url(actor.subject),
+      :links   => {
+        :profile => polymorphic_url([actor.subject, :profile]),
+        :updates_from => polymorphic_url([actor.subject, :activities], :format => :atom)
+      })
 
     self.response_body = finger.to_xml
     self.content_type  = Mime::XML
