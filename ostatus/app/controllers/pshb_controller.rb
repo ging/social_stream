9ae508d8577c1a8c09bfc2a62d76357d482b7845
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-09-09 13:37:38 +0200

    Fix TODO syntax

--- ostatus/app/controllers/pshb_controller.rb
+++ ostatus/app/controllers/pshb_controller.rb
@@ -4,7 +4,7 @@ class PshbController < ApplicationController
     #sync subscription verification
     if params['hub.mode']=='subscribe'   
       render :text => params['hub.challenge'], :status => 200
-      # TO-DO: confirm that params['hub.topic'] is a real 
+      # TODO: confirm that params['hub.topic'] is a real 
       # requested subscription by someone in this node
       return
     end
@@ -12,7 +12,7 @@ class PshbController < ApplicationController
     #sync unsubscription verification
     if params['hub.mode']=='unsubscribe'
       render :text => params['hub.challenge'], :status => 200
-      # TO-DO: confirm that params['hub.topic'] is a real 
+      # TODO: confirm that params['hub.topic'] is a real 
       # requested unsubscription by someone in this node
       # and delete permissions/remote actor if necessary
       return
