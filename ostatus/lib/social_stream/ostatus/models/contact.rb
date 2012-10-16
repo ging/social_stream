667ce15a6d4d3a06752dd7ca731964f7b08b69a1
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-10-16 10:04:17 +0200

    OStatus Unfollow support

--- ostatus/lib/social_stream/ostatus/models/contact.rb
+++ ostatus/lib/social_stream/ostatus/models/contact.rb
@@ -4,6 +4,12 @@ module SocialStream
       module Contact
         extend ActiveSupport::Concern
 
+        included do
+          # FIXME: hack, this overwrites base definition, see there
+          # for the reasons related to after_destroy callbacks
+          alias_method_chain :unset_follow_action, :salmon
+        end
+
         module ClassMethods
           # Find contact from OStatus entry
           def from_entry! entry, receiver
@@ -16,6 +22,40 @@ module SocialStream
             contact.user_author = sender
 
             contact
+          end
+        end
+
+        # Send Salmon notification
+        #
+        # FIXME DRY with activity.rb
+        def unset_follow_action_with_salmon(relation)
+          unset_follow_action_without_salmon(relation)
+
+          return if sender.subject_type == "RemoteSubject" ||
+                      receiver.subject_type != "RemoteSubject"
+
+          title = I18n.t "activity.stream.title.unfollow",
+                         author: sender_subject.name,
+                         activity_object: receiver_subject.name
+
+          entry =
+            Proudhon::Entry.new id: "tag:#{ SocialStream::Ostatus.activity_feed_host },2005:contact-destroy-#{ id }",
+                                title: title,
+                                content: title,
+                                verb: :unsubscribe,
+                                author: Proudhon::Author.new(name: sender.name,
+                                                             uri: sender.webfinger_uri)
+          salmon = entry.to_salmon
+
+          if SocialStream::Ostatus.debug_requests
+            logger.info entry.to_xml
+          end
+
+          # FIXME: Rails 4 queues
+          Thread.new do
+            salmon.deliver receiver_subject.salmon_url, sender.rsa_key
+
+            ActiveRecord::Base.connection.close
           end
         end
       end
--- ostatus/lib/social_stream/ostatus/models/tie.rb
+++ ostatus/lib/social_stream/ostatus/models/tie.rb
@@ -4,17 +4,21 @@ module SocialStream
       module Tie
         extend ActiveSupport::Concern
 
+        included do
+          after_destroy :send_salmon
+        end
+
         module ClassMethods
           # Create a new {Tie} from OStatus entry
           def create_from_entry! entry, receiver
-            contact = Contact.from_entry! entry, receiver
+            contact = ::Contact.from_entry! entry, receiver
            
             contact.relation_ids = [::Relation::Public.instance.id]
           end
 
           # Remove all {Tie} from OStatus entry
           def destroy_from_entry! entry, receiver
-            contact = Contact.from_entry! entry, receiver
+            contact = ::Contact.from_entry! entry, receiver
 
             contact.relation_ids = []
           end
