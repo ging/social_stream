6ee36539ca175a54d8fade3d9c3d3cef8e65da37
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-11-06 17:02:44 +0100

    Fix comments show and hide

--- base/app/assets/javascripts/social_stream.comments.js
+++ base/app/assets/javascripts/social_stream.comments.js
@@ -4,31 +4,52 @@
 //= require social_stream.objects
 
 SocialStream.Comments = (function(SS, $, undefined){
-	var initNew = function(){
-		// show only the text fields for new comment
-		// if there are any comment to the post
-		$(".activity_new_comment").each(function(){
-			if ($.trim($(this).siblings(".activity_comments").text()) != ""){
-				$(this).show();
-				$(this).find(".input_new_comments").val("");
-			}
-		});
+	var hideCommentEl = [
+		".actor_logo_new_comment",
+		".actor_name_new_comment",
+		".activities_comment_btn"
+	];
 
+	var initNew = function(){
+		showInputsWithComments();
 		hideNewCommentElements();
 		newCommentAutoSize();
 		newCommentClick();
 		newCommentLink();
-	}
+	};
+
+	var showInputsWithComments = function(){
+		// show only the text fields for new comment
+		// if there are any comment to the post
+		$(".activity_new_comment").each(function(){
+			if ($.trim($(this).siblings(".activity_comments").text()) !== ""){
+				$(this).find(".input_new_comments").val("");
+			} else {
+				$(this).hide();
+			}
+		});
+	};
 
 	var hideNewCommentElements = function(){
-		$(".activities_comment_btn").hide();
-		$(".actor_name_new_comment").hide();
-		$(".actor_logo_new_comment").hide();
-	}
+		$.each(hideCommentEl, function(i, selector) {
+			$(selector).hide();
+		});
+
+	};
+
+	var hideNewActivityCommentElements = function(activityId){
+		jSelector = $('#' + activityId);
+
+		$.each(hideCommentEl, function(i, selector) {
+			jSelector.find(selector).hide();
+		});
+
+		jSelector.find(".input_new_comments").hide();
+	};
 
 	var newCommentAutoSize = function(){
                 $(".input_new_comments").autosize();
-	}
+	};
 
 	var newCommentClick = function(){
 		$(".input_new_comments").click(function(){
@@ -36,14 +57,16 @@ SocialStream.Comments = (function(SS, $, undefined){
 			$(".new_comment").removeClass("new_comment_shown");
 			$(".actor_name_new_comment").hide();
 			$(".actor_logo_new_comment").hide();
+
 			var comment= $(this).parents(".activity_new_comment");
 			comment.find(".activities_comment_btn").show();
 			$(this).parents(".new_comment").addClass("new_comment_shown");
 			comment.find(".actor_name_new_comment").show();
 			comment.find(".actor_logo_new_comment").show();
+                        comment.find(".input_new_comments").show();
 			return false;
 		});
-	}
+	};
 
 	var newCommentLink = function(){
 		//javascript for tocomment option
@@ -58,11 +81,11 @@ SocialStream.Comments = (function(SS, $, undefined){
 
 		return false;
 		});
-	}
+	};
 
 	SocialStream.Timeline.addInitCallback(initNew);
 
-	SocialStream.Timeline.addCreateCallback(hideNewCommentElements);
+	SocialStream.Timeline.addCreateCallback(hideNewActivityCommentElements);
 	SocialStream.Timeline.addCreateCallback(newCommentAutoSize);
 	SocialStream.Timeline.addCreateCallback(newCommentClick);
 	SocialStream.Timeline.addCreateCallback(newCommentLink);
