c1b7a0c065a8de19fd877add0f197ab3318d5c5e
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-12-23 14:29:13 +0100

    Use inverse_of relations in the core

--- base/lib/social_stream/models/subtype.rb
+++ base/lib/social_stream/models/subtype.rb
@@ -19,7 +19,8 @@ module SocialStream #:nodoc:
         belongs_to supertype_name, {                          # belongs_to :actor, {
                     :validate  => true,                       #   :validate => true
                     :autosave  => true,                       #   :autosave => true
-                    :dependent => :destroy                    #   :dependent => :destroy
+                    :dependent => :destroy,                   #   :dependent => :destroy
+                    :inverse_of => name.underscore.to_sym     #   :inverse_of => :user,
                   }.merge(supertype_options[:belongs] || {})  #   }.merge(supertype_options[:belongs] || {})
 
         class_eval <<-EOS
