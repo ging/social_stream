4de29e0b2fdc44b1ab7cbd0631f49229ade7d0ff
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-01-17 15:33:34 +0100

    Explore tabs

--- base/app/helpers/explore_helper.rb
+++ base/app/helpers/explore_helper.rb
@@ -1,15 +1,16 @@
 module ExploreHelper
-  def explore_button_class btn
-    active =
-      case btn
-      when 'index'
-        params[:section].blank?
-      when 'search'
-        controller.controller_name == 'search'
-      else 
-        btn == params[:section]
-      end
+  def current_explore_section? s
+    case s
+    when 'explore'
+      params[:section].blank?
+    when 'search'
+      controller.controller_name == 'search'
+    else 
+      s == params[:section]
+    end
+  end
 
-   active ? 'active' : ''
+  def explore_button_class btn
+    current_explore_section?(btn) ? 'active' : ''
   end
 end
