8340072f46498f2558ff00dca5ba1de13c058a8b
Carolina <carolgarcia@dit.upm.es>
2013-01-25 14:16:30 +0100

    Improve addresses association

--- places/app/models/geotag.rb
+++ places/app/models/geotag.rb
@@ -3,18 +3,18 @@ class Geotag < ActiveRecord::Base
 	belongs_to :activity_object, :autosave => true
 	belongs_to :address, :autosave => true
 
+	before_validation :lookfor_address
+
   def address!
     address || build_address
   end
 
-  def autosave_associated_records_for_address
-    if new_address = Address.find_by_streetAddress_and_locality(address.streetAddress, address.locality) then
-      self.address = new_address
-    else
-      self.address.save!
-      self.address_id = address.id
+  private
+
+  def lookfor_address
+    if new_address = Address.find_by_streetAddress_and_locality(address.streetAddress, address.locality)
+    	self.address = new_address
     end
   end
-
 
 end
