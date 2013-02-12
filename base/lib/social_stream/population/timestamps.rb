ca394e1f2f6d0ceaf150571824e683ee0a11a2a9
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-02-12 23:44:39 +0100

    Event population

--- base/lib/social_stream/population/timestamps.rb
+++ base/lib/social_stream/population/timestamps.rb
@@ -1,11 +1,17 @@
 module SocialStream
   module Population
     class Timestamps
+      SCOPE = 1.month
+
       attr_reader :created, :updated
 
       def initialize
-        @updated = Time.at(rand(Time.now.to_i))
-        @created = Time.at(rand(@updated.to_i))
+        created = rand(SCOPE)
+        update = [ true, false ].sample
+        updated = update ? rand(created) : created
+
+        @created = Time.at(Time.now.to_i - created)
+        @updated = Time.at(Time.now.to_i - updated)
       end
     end
   end
