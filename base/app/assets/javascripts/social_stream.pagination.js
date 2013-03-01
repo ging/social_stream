4de613a208de1e898e3dc0fceb7b7ab99f9b5284
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-03-01 17:36:11 +0100

    Toggle pagination loading

--- base/app/assets/javascripts/social_stream.pagination.js
+++ base/app/assets/javascripts/social_stream.pagination.js
@@ -3,10 +3,14 @@ SocialStream.Pagination = (function(SS, $, undefined){
   var show = function(callback) {
     $(".btn-see-more").each(function(i, btn) {
       $(btn).attr('data-page', "2");
+      $(btn).closest('nav.more').find('.loading').hide();
     });
 
     $(".btn-see-more").click(function(event) {
       event.preventDefault();
+
+      $(event.target).hide();
+      $(event.target).closest('nav.more').find('.loading').show();
 
       $.ajax({
         url: $(this).attr('data-path'),
@@ -14,13 +18,15 @@ SocialStream.Pagination = (function(SS, $, undefined){
         data: { page: $(this).attr('data-page') },
         type: 'GET',
         success: function(data) {
-          $(event.target).before(data);
+          var nav = $(event.target).closest("nav.more");
+
+          nav.before(data);
+          nav.find('.loading').hide();
 
           // data.length is equal to 1
           if (data.length > 1) {
             $(event.target).attr('data-page', parseInt($(event.target).attr('data-page'), 10) + 1);
-          } else {
-            $(event.target).hide();
+            $(event.target).show();
           }
 
           if (callback) {
