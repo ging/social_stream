1d9604a44d359058dceb1c4a03b46d2c8b8102bf
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-03-25 10:35:41 +0100

    Autosize wall input

--- base/app/models/post.rb
+++ base/app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
   include SocialStream::Models::Object
 
+  MAXIMUM_LENGTH = 144
+
   alias_attribute :text, :description
-  validates_presence_of :text
+  validates :text,
+    presence: true,
+    length: { maximum: MAXIMUM_LENGTH }
 
   define_index do
     activity_object_index
