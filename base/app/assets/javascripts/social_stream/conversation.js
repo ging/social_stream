5286f758427225d944b9addee8f40d9418149416
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-04-01 15:26:13 +0200

    Use not minified version of jquery.cleditor

--- base/app/assets/javascripts/social_stream/conversation.js
+++ base/app/assets/javascripts/social_stream/conversation.js
@@ -3,7 +3,7 @@
 // cleditor requires jquery-browser
 // https://groups.google.com/forum/?fromgroups=#!topic/cleditor/5Vn4YDaQx08
 //= require jquery.browser
-//= require jquery.cleditor.min
+//= require jquery.cleditor
 
 //= require social_stream/callback
 //= require social_stream/pagination
