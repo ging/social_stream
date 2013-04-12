ef6d5dc28a53b46ab1e83d0dc70f978f2cfd3d21
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-04-12 10:27:15 +0200

    Refactorize form_privacy javascript

--- base/app/assets/javascripts/social_stream/wall.js
+++ base/app/assets/javascripts/social_stream/wall.js
@@ -1,40 +1,10 @@
 //= require jquery.autosize
 //
 //= require social_stream/callback
+//= require social_stream/object
 
 SocialStream.Wall = (function(SS, $, undefined){
   var callback = new SS.Callback();
-
-  var initRelationSelect = function(options){
-    $('.wall_input select[name*="relation_ids"]').multiselect({
-      'buttonClass': 'btn btn-small',
-      'buttonText': relationSelectText
-    });
-  };
-
-  var relationSelectText = function(options) {
-    var text;
-
-    if (options.length === 0) {
-      text = $(".wall_input").attr('data-relation-text');
-    }
-    else if (options.length > 3) {
-      text = I18n.t('activity.privacy.relation', { count: options.length });
-    } else {
-      var selected = '';
-      options.each(function() {
-        selected += $(this).text() + ', ';
-      });
-
-      text = selected.substr(0, selected.length - 2);
-    }
-
-    return text + ' <b class="caret"></b>';
-  };
-
-  var changeRelationSelect = function(type) {
-    $('.wall_input [name$="[relation_ids][]"]').attr('name', type + '[relation_ids][]');
-  };
 
 
   var initInputAutosize = function() {
@@ -62,13 +32,13 @@ SocialStream.Wall = (function(SS, $, undefined){
 
   callback.register('show',
                     initInputAutosize,
-                    initRelationSelect);
+                    SS.Object.new_);
 
   callback.register('new_',
                     resetWallInput);
 
   return callback.extend({
     changeAction: changeAction,
-    changeRelationSelect: changeRelationSelect 
+    changeRelationSelect: SS.Object.changeRelationSelect 
   });
 }) (SocialStream, jQuery);
