7e4ea1ad290e104b585732ede3bc3fb65cb6d5bc
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-05-28 09:30:04 +0200

    Improve initializer doc

--- base/lib/generators/social_stream/base/templates/initializer.rb
+++ base/lib/generators/social_stream/base/templates/initializer.rb
@@ -1,4 +1,6 @@
 SocialStream.setup do |config|
+  ## Subjects
+
   # List the models that are social entities. These will have ties between them.
   # Remember you must add an "actor_id" foreign key column to your migration!
   #
@@ -10,17 +12,7 @@ SocialStream.setup do |config|
   # config.devise_modules = :database_authenticatable, :registerable,
   #                         :recoverable, :rememberable, :trackable,
   #                         :omniauthable, :token_authenticatable
-
-  # Type of activities managed by actors
-  # Remember you must add an "activity_object_id" foreign key column to your migration!
-  #
-  # config.objects += [ :foo, :bar ]
-
-  # Form for activity objects to be loaded
-  # You can write your own activity objects
-  #
-  # config.activity_forms = [ :post, :document, :foo, :bar ]
-
+  
   # Config the relation model of your network
   #
   # :custom - users define their own relation types, and post with privacy, like Google+
@@ -31,20 +23,36 @@ SocialStream.setup do |config|
   # Configure the type of actors that are suggested in the sidebar
   #
   # config.suggested_models = [ :user, :group ]
-  
-  # Configure the models that will appear in the repository tab
+ 
+  ## Objects
+
+  # Activities objects managed by actors
+  # Remember you must add an "activity_object_id" foreign key column to your migration!
+  #
+  # config.objects += [ :foo, :bar ]
+
+  # Activity objects included in the wall input form
+  # You can write your own view in app/views/your_objects/_new_activity.html.erb
+  #
+  # config.activity_forms = [ :post, :document, :foo, :bar ]
+ 
+  # Objects that appear in the repository tab
+  #
+  # You must create a vew in app/views/your_objects/_your_object.hmtl.erb
   #
   # config.repository_models = [ :document, :event, :link, :place ]
 
+  # Quick search (header) and Extended search models and its order. Remember to create
+  # the indexes with thinking-sphinx and the views at
+  # app/views/my_objects/_quick_search_result.html.erb and
+  # app/views/my_objects/_search_result.html.erb 
+  #
+  # config.quick_search_models += [:foo, :bar]
+  # config.extended_search_models += [:foo, :bar]
+  
   # Expose resque interface to manage background tasks at /resque
   #
   # config.resque_access = true
-
-  # Quick search (header) and Extended search models and its order. Remember to create
-  # the indexes with thinking-sphinx if you are using customized models.
-  #
-  # config.quick_search_models = [:user, :group]
-  # config.extended_search_models = [:user, :group]
 
   # Cleditor controls. It is used in new message editor, for example
   # config.cleditor_controls = "bold italic underline strikethrough subscript superscript | size style | bullets | image link unlink"
