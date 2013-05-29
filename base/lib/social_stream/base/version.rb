dc850ea24b47ba0fc198e57fa48090b5be468b7e
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-05-29 16:05:18 +0200

    2.0.2

--- base/lib/social_stream/base/version.rb
+++ base/lib/social_stream/base/version.rb
@@ -1,5 +1,5 @@
 module SocialStream
   module Base
-    VERSION = "2.0.1".freeze
+    VERSION = "2.0.2".freeze
   end
 end
--- presence/lib/social_stream/presence/version.rb
+++ presence/lib/social_stream/presence/version.rb
@@ -1,5 +1,5 @@
 module SocialStream
   module Presence
-    VERSION = "2.0.0"
+    VERSION = "2.0.1"
   end
 end
