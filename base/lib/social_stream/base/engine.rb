2f236f7a2e42a27e6ff60e7f04431b38a7ced867
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-05-31 15:03:35 +0200

    Fix namespace problem

--- base/lib/social_stream/base/engine.rb
+++ base/lib/social_stream/base/engine.rb
@@ -61,7 +61,7 @@ module SocialStream
 
         # Load Relation::Public, so it is registered as descendant of Relation::Single
         # and used in ActivityObject#allowed_relations
-        Relation::Public
+        ::Relation::Public
       end
     end
   end
