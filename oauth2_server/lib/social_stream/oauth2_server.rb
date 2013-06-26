33bcb0e1dabe79c409a58273d3c1da50ef62334b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-06-26 17:29:03 +0200

    Switch oauth2_server load file

--- oauth2_server/lib/social_stream/oauth2_server.rb
+++ oauth2_server/lib/social_stream/oauth2_server.rb
@@ -1,5 +1,32 @@
-# Bundler 1.2 tries to load this file instead of the gem name,
-# so we delegate to it
-#
-# We will probably change this in the future
-require 'social_stream-oauth2_server'
+require 'social_stream-base'
+
+require 'rack/oauth2'
+
+module SocialStream
+  module Oauth2Server
+    module Controllers
+      autoload :Helpers,  'social_stream/oauth2_server/controllers/helpers'
+    end
+
+    module Models
+      autoload :Actor, 'social_stream/oauth2_server/models/actor'
+      autoload :User, 'social_stream/oauth2_server/models/user'
+    end
+
+    autoload :Ability,       'social_stream/oauth2_server/ability'
+    autoload :TokenEndpoint, 'social_stream/oauth2_server/token_endpoint'
+  end
+
+  # Define default custom relations for Site::Client
+  SocialStream.custom_relations['site/client'] = {}
+
+  # Relations that appear in the Site::Client list
+  SocialStream.list_relations['site/client'] = [ :manager ]
+
+  SocialStream.available_permissions['site/client'] = [
+    [ 'manage', nil ],
+    [ 'manage', 'relation/custom' ]
+  ]
+end
+
+require 'social_stream/oauth2_server/engine'
