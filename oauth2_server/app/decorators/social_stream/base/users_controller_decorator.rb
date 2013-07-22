9f3fb0caaf08239ef293c9699a51dadec899dfad
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-07-22 14:45:33 +0200

    Remove binding.pry call

--- oauth2_server/app/decorators/social_stream/base/users_controller_decorator.rb
+++ oauth2_server/app/decorators/social_stream/base/users_controller_decorator.rb
@@ -1,6 +1,5 @@
 UsersController.class_eval do
   def current
-    binding.pry
     respond_to do |format|
       format.json { render json: current_user.to_json(client: oauth2_token.try(:client)) }
     end
