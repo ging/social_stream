1886a5a0110eb2119c8a1d0c9d1590bb7a7e98cb
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-07-26 17:07:52 +0200

    Fix db:populate

--- base/db/migrate/20130723133530_actor_notification_settings.rb
+++ base/db/migrate/20130723133530_actor_notification_settings.rb
@@ -3,5 +3,8 @@ class ActorNotificationSettings < ActiveRecord::Migration
     change_table :actors do |t|
       t.string "notification_settings"
     end
+
+    # rake db:populate does not work without this
+    Actor.reset_column_information
   end
 end
