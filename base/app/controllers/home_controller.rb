5781ecea44a32159c856085097b28ba1bdc69c54
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-09-24 17:10:43 +0200

    home.js
    Fixes #461

--- base/app/controllers/home_controller.rb
+++ base/app/controllers/home_controller.rb
@@ -1,3 +1,18 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: home_json }
+    end
+  end
+
+  private
+
+  def home_json
+    {
+      name: current_subject.name
+    }.to_json
+  end
 end
