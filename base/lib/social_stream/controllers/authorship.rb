a0b1c0de453bc51cccbaf5c27c7254077cf3c559
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-09-25 10:39:06 +0200

    Add owner_id to authorship params

--- base/lib/social_stream/controllers/authorship.rb
+++ base/lib/social_stream/controllers/authorship.rb
@@ -10,8 +10,9 @@ module SocialStream
       private
 
       def set_author_ids
-        resource_params.first[:author_id] = current_subject.try(:actor_id)
+        resource_params.first[:author_id]      = current_subject.try(:actor_id)
         resource_params.first[:user_author_id] = current_user.try(:actor_id)
+        resource_params.first[:owner_id]       ||= current_subject.try(:actor_id)
       end
     end
   end
